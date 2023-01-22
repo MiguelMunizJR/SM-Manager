48d79ea53dfda8c883f03b027fdb333ec3bf4303
Miguel Muñiz <73082309+MiguelMunizJR@users.noreply.github.com>
2023-01-22 00:23:43 -0600

    start with tailwind and config complete!

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "commonjs": true,
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "overrides": [
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": 0,
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "react/react-in-jsx-scope": "off",
+  }
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="w-full min-h-screen flex justify-center bg-blue-100">
+      <nav className="w-full h-14 m-3 bg-gray-400 rounded flex justify-between content-center">
+        <h1 className="">Hello World with TailwindCSS!</h1>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
