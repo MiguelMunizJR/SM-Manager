b328325b1dea280d1eae442e0c99035e81ddf2a8
Miguel Muñiz <73082309+MiguelMunizJR@users.noreply.github.com>
2023-02-21 02:41:31 -0600

    login and register component done!

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,27 +1,111 @@
-import Header from "../container/Header";
+import { NavLink } from "react-router-dom";
+import TimelineNav from "../../utilities/container/TimelineNav";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
-const Login = ({ setShowSideBar, showSideBar, activePage }) => {
+const Login = () => {
+  const { register, handleSubmit, reset } = useForm();
+
+  const submitForm = (data) => {
+    const URL = "https://crud-api-express.onrender.com/api/v1/";
+
+    axios
+      .post(URL, data)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    reset({
+      email: "",
+      password: "",
+    });
+  };
+
   return (
     <>
       <section className="w-full h-screen flex flex-col bg-gray-50">
-        <Header setShowSideBar={setShowSideBar}
-          showSideBar={showSideBar} activePage={activePage} />
-        <article className="pl-4 font-default text-gray-800">
-          <div className="mt-4 flex gap-2 font-default text-lg font-medium text-gray-600">
-            <i className="fa-solid fa-house text-base"></i>
-            <h2>Home</h2>
-          </div>
-          <h4 className="mt-5 text-xl font-medium text-blue-600">
-            Welcome, <span className="text-gray-800">Junior</span>
-          </h4>
-          <h2 className="mt-2 pl-4 font-medium text-xl text-gray-700">
-            Manage your <span className="text-blue-600">clients</span> or{" "}
-            <span className="text-blue-600">tasks</span> for your day-to-day
-            operations
-          </h2>
+        <article className="w-full h-16 md:mt-3 flex justify-between items-center bg-gray-50 z-40 md:bg-transparent">
+          <section className="w-full md:hidden ml-5 flex gap-4 items-center text-gray-800">
+            <NavLink to="/">
+              <h1 className="py-2 font-default text-xl font-semibold">
+                SM Manager
+              </h1>
+            </NavLink>
+          </section>
         </article>
-        <section className="w-11/12 min-h-max mt-7 mx-auto flex flex-col gap-10 font-default">
-          <h1>Hola</h1>
+        <TimelineNav
+          actualPage="Login"
+          actualIcon="fa-solid fa-key"
+          prevPag="Home"
+          prevIcon="fa-solid fa-home"
+        />
+        <section className="w-full h-full flex items-center justify-center">
+          <article className="w-5/6 h-full mt-24 font-default flex flex-col">
+            <h4 className="text-xs text-blue-500 font-semibold">Welcome</h4>
+            <h2 className="mt-2 text-gray-800 font-medium text-3xl">Login</h2>
+            <h4 className="mt-2 text-sm font-medium text-gray-400">
+              Please sign in to continue
+            </h4>
+            <form className="w-full min-h-max mt-8 flex flex-col gap-4 justify-center items-center" onSubmit={handleSubmit(submitForm)}>
+              <div className="w-full min-h-max flex mx-auto flex-col gap-2 p-2 justify-center">
+                <label
+                  htmlFor="email"
+                  className="font-semibold text-gray-900 text-sm"
+                >
+                  Email: *
+                </label>
+                <input
+                  id="email"
+                  type="email"
+                  placeholder="Email"
+                  className="w-full h-10 bg-transparent border-b-2 border-b-gray-300 outline-none transition-all duration-150 focus:border-blue-500 text-lg font-normal text-gray-700"
+                  {...register("email")}
+                  required
+                />
+              </div>
+              <div className="w-full min-h-max flex mx-auto flex-col gap-2 p-2 justify-center">
+                <label
+                  htmlFor="password"
+                  className="font-semibold text-gray-900 text-sm"
+                >
+                  Password: *
+                </label>
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                  className="w-full h-10 bg-transparent border-b-2 border-b-gray-300 outline-none transition-all duration-150 focus:border-blue-500 text-lg font-normal text-gray-700"
+                  {...register("password")}
+                  required
+                />
+              </div>
+              <p className="text-sm font-medium text-gray-400 self-end flex items-center gap-1">
+                <i className="fa-solid fa-key"></i>
+                Forgot Password
+              </p>
+              <button className="w-1/2 h-10 flex justify-center items-center gap-2 self-end mt-4 font-medium rounded drop-shadow-lg text-gray-50 bg-blue-500">
+                Login
+                <i className="fa-solid fa-arrow-right-long"></i>
+              </button>
+              <div className="w-full h-min-max mt-2 flex py-5 items-center">
+                <div className="flex-grow border-t border-gray-300"></div>
+                <span className="flex-shrink mx-4 text-gray-300">
+                  Or login with
+                </span>
+                <div className="flex-grow border-t border-gray-300"></div>
+              </div>
+            </form>
+            <h5 className="mt-4 mx-auto text-sm font-medium text-gray-500">
+              don&apos;t have an account?
+              <NavLink to="/register" className="font-semibold text-blue-500">
+                {" "}
+                Sign up
+              </NavLink>
+            </h5>
+          </article>
         </section>
       </section>
     </>
--- src/components/login/Register.jsx
+++ src/components/login/Register.jsx
@@ -1,7 +1,139 @@
+import { NavLink } from "react-router-dom";
+import TimelineNav from "../../utilities/container/TimelineNav";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
 const Register = () => {
+  const { register, handleSubmit, reset } = useForm();
+
+  const submitForm = (data) => {
+    const URL = "";
+
+    axios
+      .post(URL, data)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    reset({
+      email: "",
+      password: "",
+    });
+  };
+
   return (
-    <div>Register</div>
+    <>
+      <section className="w-full h-screen flex flex-col bg-gray-50">
+        <article className="w-full h-16 md:mt-3 flex justify-between items-center bg-gray-50 z-40 md:bg-transparent">
+          <section className="w-full md:hidden ml-5 flex gap-4 items-center text-gray-800">
+            <NavLink to="/">
+              <h1 className="py-2 font-default text-xl font-semibold">
+                SM Manager
+              </h1>
+            </NavLink>
+          </section>
+        </article>
+        <TimelineNav
+          actualPage="Sign up"
+          actualIcon="fa-solid fa-key"
+          prevPag="Home"
+          prevIcon="fa-solid fa-home"
+        />
+        <section className="w-full h-full flex items-center justify-center">
+          <article className="w-5/6 h-full mt-24 font-default flex flex-col">
+            <h4 className="text-xs text-blue-500 font-semibold">Welcome</h4>
+            <h2 className="mt-2 text-gray-800 font-medium text-3xl">Sign up</h2>
+            <form className="w-full min-h-max mt-8 flex flex-col gap-4 justify-center items-center" onSubmit={handleSubmit(submitForm)}>
+              <div className="w-full min-h-max flex mx-auto flex-col gap-1 p-2 justify-center">
+                <label
+                  htmlFor="firstName"
+                  className="font-semibold text-gray-900 text-sm"
+                >
+                  First Name: *
+                </label>
+                <input
+                  id="firstName"
+                  type="text"
+                  placeholder="First name"
+                  className="w-full h-10 bg-transparent border-b-2 border-b-gray-300 outline-none transition-all duration-150 focus:border-blue-500 text-lg font-normal text-gray-700"
+                  {...register("firstName")}
+                  required
+                />
+              </div>
+              <div className="w-full min-h-max flex mx-auto flex-col gap-1 p-2 justify-center">
+                <label
+                  htmlFor="lastName"
+                  className="font-semibold text-gray-900 text-sm"
+                >
+                  Last Name: *
+                </label>
+                <input
+                  id="lastName"
+                  type="text"
+                  placeholder="Last name"
+                  className="w-full h-10 bg-transparent border-b-2 border-b-gray-300 outline-none transition-all duration-150 focus:border-blue-500 text-lg font-normal text-gray-700"
+                  {...register("lastName")}
+                  required
+                />
+              </div>
+              <div className="w-full min-h-max flex mx-auto flex-col gap-1 p-2 justify-center">
+                <label
+                  htmlFor="email"
+                  className="font-semibold text-gray-900 text-sm"
+                >
+                  Email: *
+                </label>
+                <input
+                  id="email"
+                  type="email"
+                  placeholder="Email"
+                  className="w-full h-10 bg-transparent border-b-2 border-b-gray-300 outline-none transition-all duration-150 focus:border-blue-500 text-lg font-normal text-gray-700"
+                  {...register("email")}
+                  required
+                />
+              </div>
+              <div className="w-full min-h-max flex mx-auto flex-col gap-1 p-2 justify-center">
+                <label
+                  htmlFor="password"
+                  className="font-semibold text-gray-900 text-sm"
+                >
+                  Password: *
+                </label>
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                  className="w-full h-10 bg-transparent border-b-2 border-b-gray-300 outline-none transition-all duration-150 focus:border-blue-500 text-lg font-normal text-gray-700"
+                  {...register("password")}
+                  required
+                />
+              </div>
+              <button className="w-1/2 h-10 flex justify-center items-center gap-2 self-end mt-4 font-medium rounded drop-shadow-lg text-gray-50 bg-blue-500">
+                Sign up
+                <i className="fa-solid fa-arrow-right-long"></i>
+              </button>
+              <div className="w-full h-min-max mt-2 flex py-5 items-center">
+                <div className="flex-grow border-t border-gray-300"></div>
+                <span className="flex-shrink mx-4 text-gray-300">
+                  Or sign up with
+                </span>
+                <div className="flex-grow border-t border-gray-300"></div>
+              </div>
+            </form>
+            <h5 className="mt-4 mx-auto text-sm font-medium text-gray-500">
+              Already have a account?
+              <NavLink to="/login" className="font-semibold text-blue-500">
+                {" "}
+                Login
+              </NavLink>
+            </h5>
+          </article>
+        </section>
+      </section>
+    </>
   );
 };
 
