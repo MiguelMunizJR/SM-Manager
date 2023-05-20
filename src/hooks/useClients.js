4c2498133951623c1531aae6e754d35723b4adcc
Miguel Muñiz <73082309+MiguelMunizJR@users.noreply.github.com>
2023-05-20 00:21:35 -0600

    new hook for clients

---
+++ src/hooks/useClients.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import { ROUTES_PATH, URL_API } from "../consts";
+import getConfig from "../utilities/getConfig";
+
+const useClients = () => {
+  const [clients, setClients] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const getAllClients = () => {
+    const URL = `${URL_API}${ROUTES_PATH.CLIENTS}`;
+
+    axios
+      .get(URL, getConfig())
+      .then((res) => {
+        setClients(res.data?.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  };
+
+  useEffect(() => {
+    getAllClients();
+  }, []);
+  
+  
+  return { clients, loading, error, getAllClients };
+};
+
+export default useClients;
