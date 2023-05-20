5d89052e50e2a364e102a2489b8100ab9d0a92f2
Miguel Muñiz <73082309+MiguelMunizJR@users.noreply.github.com>
2023-05-20 00:24:24 -0600

    changed magic strings

--- src/utilities/home/itemsHome.js
+++ src/utilities/home/itemsHome.js
@@ -1,9 +1,11 @@
+import { ROUTES_PATH } from "../../consts";
+
 const itemsHome = [
   {
     id: 1,
     title: "Clients",
     icon: "fa-solid fa-users",
-    route: "/clients",
+    route: ROUTES_PATH.CLIENTS,
     image: "https://i.postimg.cc/HkgpDkTX/clients-home.png",
     className:
       "w-full md:max-w-lg h-52 py-4 flex flex-col rounded-lg text-gray-50 bg-gradient-to-bl from-blue-600 to-sky-400 drop-shadow-lg hover:drop-shadow-2xl cursor-pointer transition-all duration-250 hover:transform hover:scale-105",
@@ -12,7 +14,7 @@ const itemsHome = [
     id: 2,
     title: "Tasks",
     icon: "fa-solid fa-list-check",
-    route: "/tasks",
+    route: ROUTES_PATH.TASKS,
     image: "https://i.postimg.cc/Wbr2Hkcs/tasks-home.png",
     className:
       "w-full md:max-w-lg h-52 py-4 flex flex-col rounded-lg text-gray-50 bg-gradient-to-bl from-orange-500 to-yellow-300 drop-shadow-lg hover:drop-shadow-2xl cursor-pointer transition-all duration-250 hover:transform hover:scale-105",
--- src/utilities/navbar/itemsNav.js
+++ src/utilities/navbar/itemsNav.js
@@ -1,26 +1,28 @@
+import { ROUTES_PATH } from "../../consts";
+
 const itemsNav = [
   {
     id: 1,
     title: "Home",
-    route: "/",
+    route: ROUTES_PATH.HOME,
     icon: "fa-solid fa-house",
   },
   // {
   //   id: 2,
   //   title: "Account",
-  //   route: "/users/me",
+  // route: ROUTES_PATH.USER,
   //   icon: "fa-solid fa-user",
   // },
   {
     id: 3,
     title: "Clients",
-    route: "/clients",
+    route: ROUTES_PATH.CLIENTS,
     icon: "fa-solid fa-users",
   },
   {
     id: 4,
     title: "Tasks",
-    route: "/tasks",
+    route: ROUTES_PATH.TASKS,
     icon: "fa-solid fa-list-check",
   },
   // {
