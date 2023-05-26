aa890959cd9e5e2a5b5574155988e7f00d9671ca
Miguel Muñiz <73082309+MiguelMunizJR@users.noreply.github.com>
2023-05-26 00:07:13 -0600

    change animation-spin

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
-    <div className="w-screen h-screen flex justify-center items-center fixed z-50 bg-gray-200">
-      <div className="w-14 h-14 rounded-full border-y-4 border-x-4 border-t-blue-500 animate-spin"></div>
+    <div className="w-screen h-screen flex justify-center items-center fixed z-50 bg-slate-100">
+      <div className="w-14 h-14 rounded-full border-y-4 border-x-4 border-t-blue-500 animate-reload"></div>
     </div>
   );
 };
